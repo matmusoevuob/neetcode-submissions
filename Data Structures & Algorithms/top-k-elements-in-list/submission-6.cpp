31a0d02c5b6175ac742aba522372b88d889f3b3a
Muhammadjon Matmusoev <mxm1833@student.bham.ac.uk>
2026-08-07 11:41:50 +0500

    Add: top-k-elements-in-list - submission-6

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-6.cpp
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> freq;
+        for (int num : nums) ++freq[num];
+
+        vector<pair<int, int>> pairs(freq.begin(), freq.end());
+        sort(pairs.begin(), pairs.end(), [](const auto& a, const auto& b) {
+            return a.second > b.second;
+        });
+
+        vector<int> result;
+        for (int i = 0; i < k && i < static_cast<int>(pairs.size()); ++i) {
+            result.push_back(pairs[i].first);
+        }
+        return result;
+    }
+};
